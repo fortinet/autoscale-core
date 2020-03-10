89f656a0bdad544e4ab2918e78f34368af5dd8c5
Jayden Liang <jliang01@fortinet.com>
2020-03-10 11:34:46 -0700

    update description on Jenkins/ci.groovy
    Change-Id: I4700f021d75500ebc9d42fcd950b40d2f7ba57f8

--- jenkins/ci.groovy
+++ jenkins/ci.groovy
@@ -2,7 +2,6 @@ node('devops-aws') {
     stage('Clean up') {
         sh 'rm -rf *'
     }
-
     stage('Checkout Changes') {
         def changeBranch = "change-${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}"
         def scmVars = checkout scm
@@ -10,24 +9,20 @@ node('devops-aws') {
         sh "git fetch origin ${GERRIT_REFSPEC}:${changeBranch}"
         sh "git checkout ${changeBranch}"
     }
-
     stage('Install NPM Dependency') {
         echo 'NPM install..'
         sh 'npm install'
     }
-
     stage('Check NPM Dependency Vulnerability') {
         echo 'running npm audit..'
         sh 'npm audit'
     }
-
     stage('Analyze Source Code') {
         echo 'running linter..'
         sh 'npm run lint-check'
     }
-
     stage('Test') {
-        echo 'run testing..'
+        echo 'running test..'
         sh 'npm test'
     }
 }
