f1dbd2b6d2554a71d30215551cc5bd32ac7d25df
Jayden Liang <jliang01@fortinet.com>
2021-02-28 14:26:19 -0800

    improvement for indexing export files

--- index.ts
+++ index.ts
@@ -5,19 +5,11 @@ import * as DBDef from './db-definitions';
 
 export * from './autoscale-core';
 export * from './autoscale-environment';
+export * from './autoscale-service-provider';
 export * from './autoscale-setting';
 export * from './cloud-function-proxy';
-export * from './context-strategy/autoscale-context';
-export * from './context-strategy/bootstrap-context';
-export * from './context-strategy/licensing-context';
-export * from './context-strategy/nic-attachment-context';
-export * from './context-strategy/scaling-group-context';
-export * from './context-strategy/vpn-attachment-context';
+export * from './context-strategy';
 export * from './fortigate-autoscale';
-/**
- * export fortigate-autoscale modules
- */
-export * from './fortigate-autoscale/index';
 export * from './helper-function';
 export * from './jsonable';
 export * from './platform-adaptee';
@@ -35,4 +27,3 @@ export * from './primary-election';
 export * from './test-helper';
 export * from './virtual-machine';
 export { DBDef };
-
