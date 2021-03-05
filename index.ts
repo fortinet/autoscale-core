b4fb1995a91570699200e6e6f834691821f35d20
Jayden Liang <jliang01@fortinet.com>
2021-03-05 10:21:36 -0800

    remove exports from index which cause errors after npm prune --production

--- index.ts
+++ index.ts
@@ -15,15 +15,5 @@ export * from './jsonable';
 export * from './platform-adaptee';
 export * from './platform-adapter';
 export * from './primary-election';
-/**
- * export scripts
- */
-// TODO: temporarily commented out the export for code-packman module. If this file isn't needed at
-// all, then remove this line permanently.
-// export * from './scripts/code-packman';
-/**
- * export test-helpers
- */
-export * from './test-helper';
 export * from './virtual-machine';
 export { DBDef };
